d79c196a3b7e36f0443f344564edb05455307579
Nia <nahiayeseniamontoyasanchez@gmail.com>
2025-04-25 19:39:17 -0500

    Update Program.cs

--- POWWEB/Program.cs
+++ POWWEB/Program.cs
@@ -24,4 +24,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 app.Run();
